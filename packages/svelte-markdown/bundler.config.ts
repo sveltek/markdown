e36c15deb1367cdc408a48613c3be902aab2850c
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-01 15:11:13 +0100

    refactor: add packages

---
+++ packages/svelte-markdown/bundler.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from '@hypernym/bundler'
+
+export default defineConfig({
+  entries: [
+    {
+      input: './src/index.ts',
+    },
+    {
+      dts: './src/types.ts',
+      output: './dist/index.d.mts',
+    },
+  ],
+})
