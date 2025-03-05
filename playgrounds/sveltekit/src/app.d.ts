7a415abba39ed277993f33848058a6675e2b58dc
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-05 13:05:55 +0100

    types: improve global types for markdown files

--- playgrounds/sveltekit/src/app.d.ts
+++ playgrounds/sveltekit/src/app.d.ts
@@ -4,6 +4,14 @@ declare global {
     // interface Locals {}
     // interface PageData {}
     // interface Platform {}
+
+    declare module '*.md' {
+      import type { Component } from 'svelte'
+
+      declare const MarkdownComponent: Component
+
+      export default MarkdownComponent
+    }
   }
 }
 
