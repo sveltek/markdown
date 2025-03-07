b454d642e4fb418d56bd495145943f8fe78df0d2
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-07 16:27:42 +0100

    chore: add default layout to playground

---
+++ playgrounds/sveltekit/src/content/layouts/default/layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+
+  interface Props {
+    children: Snippet
+    frontmatter: {
+      layout?: string | false
+      title?: string
+    }
+  }
+
+  let { children, frontmatter }: Props = $props()
+</script>
+
+<svelte:head>
+  <title>{frontmatter.title}</title>
+</svelte:head>
+
+<h1>{frontmatter.title}</h1>
+
+<div id={frontmatter.layout ? `content-${frontmatter.layout}` : ''}>
+  {@render children?.()}
+</div>
