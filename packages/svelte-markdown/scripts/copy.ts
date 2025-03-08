97ad5e1df287f3a044b0aa62a9e633cd14c69de5
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-08 15:29:09 +0100

    chore: update scripts

--- packages/svelte-markdown/scripts/copy.ts
+++ packages/svelte-markdown/scripts/copy.ts
@@ -1,6 +1,6 @@
 import { cwd } from 'node:process'
 import { resolve } from 'node:path'
-import { copy } from '@hypernym/utils/fs'
+import { cp } from 'node:fs/promises'
 
 /**
  * Copies `README.md` file from the `workspace` to the `package` before publishing.
@@ -9,7 +9,9 @@ async function copyReadme(): Promise<void> {
   try {
     const root = cwd()
 
-    await copy(resolve(root, '../../README.md'), resolve(root, './README.md'))
+    await cp(resolve(root, '../../README.md'), resolve(root, './README.md'), {
+      recursive: true,
+    })
 
     console.log('Copying completed successfully!')
   } catch (err) {
