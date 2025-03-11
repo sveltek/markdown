b37e074109c1d753350154eb3958160bc92b9304
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-11 16:32:00 +0100

    refactor: add svelte instance fn in compile

---
+++ packages/svelte-markdown/src/compile/instance.ts
@@ -0,0 +1,36 @@
+import { resolve, relative } from 'node:path'
+import { print } from 'esrap'
+import { meta } from '@/utils'
+import type { AST } from 'svelte/compiler'
+import type { ASTScript } from './types'
+
+const posix = (path: string): string => {
+  const isExtendedLengthPath = /^\\\\\?\\/.test(path)
+  const hasNonAscii = /[^\0-\x80]+/.test(path)
+  if (isExtendedLengthPath || hasNonAscii) return path
+  return path.replace(/\\/g, '/')
+}
+
+const getRelativePath = (from: string, to: string): string => {
+  const path = posix(relative(resolve(from, '..'), to))
+  return path.startsWith('.') ? path : `./${path}`
+}
+
+export function createSvelteInstance(
+  instance: AST.Root['instance'],
+  filePath: string,
+  layoutPath: string,
+): ASTScript {
+  const path = getRelativePath(filePath, layoutPath)
+
+  const imports = `import ${meta.layoutName}, * as ${meta.componentName} from "${path}";\n`
+
+  if (!instance) {
+    const content = `<script>\n${imports}</script>\n`
+    return { start: 0, end: 0, content }
+  }
+
+  const content = `<script>\n${imports}${print(instance.content).code}\n</script>\n`
+
+  return { start: instance.start, end: instance.end, content }
+}
