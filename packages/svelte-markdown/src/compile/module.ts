4f5b6df45fa5bbed9ebd33eaca24448feb7df66a
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-11 16:30:39 +0100

    refactor: add svelte module fn in compile

---
+++ packages/svelte-markdown/src/compile/module.ts
@@ -0,0 +1,24 @@
+import { print } from 'esrap'
+import type { AST } from 'svelte/compiler'
+import type { FileData, ASTScript } from './types'
+
+export function createSvelteModule(
+  module: AST.Root['module'],
+  data: FileData,
+): ASTScript {
+  const keys = Object.keys(data.frontmatter!)
+
+  let frontmatter = `export const frontmatter = ${JSON.stringify(data.frontmatter)};\n`
+  if (keys.length) {
+    frontmatter += `const { ${keys.join(', ')} } = frontmatter;\n`
+  }
+
+  if (!module) {
+    const content = `<script module>\n${frontmatter}</script>\n`
+    return { start: 0, end: 0, content }
+  }
+
+  const content = `<script module>\n${frontmatter}${print(module.content).code}\n</script>\n`
+
+  return { start: module.start, end: module.end, content }
+}
