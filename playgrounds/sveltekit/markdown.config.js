649366f064f989f9a5d16d28642af978d425d8bb
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-12 19:05:25 +0100

    chore: add code highlighter to playground

--- playgrounds/sveltekit/markdown.config.js
+++ playgrounds/sveltekit/markdown.config.js
@@ -1,4 +1,11 @@
+import { createHighlighter } from 'shiki'
 import { defineConfig } from '../../packages/svelte-markdown/dist/index.mjs'
+
+const theme = 'github-dark-default'
+const highlighter = await createHighlighter({
+  themes: [theme],
+  langs: ['javascript', 'typescript', 'svelte'],
+})
 
 export const markdownConfig = defineConfig({
   frontmatter: {
@@ -13,6 +20,11 @@ export const markdownConfig = defineConfig({
   layouts: {
     default: {
       path: 'src/content/layouts/default/layout.svelte',
+    },
+  },
+  highlight: {
+    highlighter: async ({ lang, code }) => {
+      return highlighter.codeToHtml(code, { lang, theme })
     },
   },
 })
