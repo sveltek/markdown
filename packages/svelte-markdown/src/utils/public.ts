e3450f2b3cbcedd282a7c9395b647a4d526cbb9f
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-13 13:59:34 +0100

    update utils dir

--- packages/svelte-markdown/src/utils/public.ts
+++
@@ -1,7 +0,0 @@
-export const escapeSvelte = (value: string): string =>
-  value
-    .replace(
-      /[{}`]/g,
-      (v) => ({ '{': '&#123;', '}': '&#125;', '`': '&#96;' })[v] || v,
-    )
-    .replace(/\\([trn])/g, '&#92;$1')
