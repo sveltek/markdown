af8c3728dfa5cc156b9a5d6398885a7f1adedd5e
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-13 14:12:44 +0100

    chore: update main bundler config

--- packages/svelte-markdown/bundler.config.ts
+++ packages/svelte-markdown/bundler.config.ts
@@ -1,11 +1,29 @@
-import { defineConfig, externals } from '@hypernym/bundler'
+import { defineConfig, externals, resolvePaths } from '@hypernym/bundler'
 import { dependencies } from './package.json'
 
 export default defineConfig({
   entries: [
+    // Utils
+    {
+      input: './src/utils/public/index.ts',
+      output: './dist/utils/index.mjs',
+    },
+    {
+      dts: './src/utils/public/types.ts',
+      output: './dist/utils/index.d.mts',
+    },
+    // Main
     {
       input: './src/index.ts',
-      externals: [...externals, ...Object.keys(dependencies), /^svelte/],
+      externals: [
+        ...externals,
+        ...Object.keys(dependencies),
+        /^svelte/,
+        '@/utils/public',
+      ],
+      paths: resolvePaths([
+        { find: '@/utils/public', replacement: './utils/index.mjs' },
+      ]),
     },
     {
       dts: './src/types/index.ts',
