a90f1dd38e34590379a561c0886617097f3145c1
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-17 17:37:09 +0100

    feat: add new remark-toc plugin

--- packages/svelte-markdown/src/plugins/public/types.ts
+++ packages/svelte-markdown/src/plugins/public/types.ts
@@ -12,3 +12,5 @@ export type * as Unified from 'unified'
 export type * as VFile from 'vfile'
 export type * as Mdast from 'mdast'
 export type * as Hast from 'hast'
+
+export * from './remark/types'
