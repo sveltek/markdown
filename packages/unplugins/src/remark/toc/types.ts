21dee4e163a8e0f732a3df48a24a72ed77721979
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-29 15:19:25 +0100

    feat: add new unplugins package

---
+++ packages/unplugins/src/remark/toc/types.ts
@@ -0,0 +1,22 @@
+export interface TocOptions {
+  /**
+   * Specifies the maximum headings depth to be included in the table of content.
+   *
+   * @default 3
+   */
+  depth?: number
+  /**
+   * Specifies whether headings include link tags.
+   *
+   * @default true
+   */
+  links?: boolean
+}
+
+export interface TocItem {
+  id: string
+  depth: number
+  value: string
+}
+
+export type TocItems = TocItem[]
---
+++ packages/unplugins/src/types.ts
@@ -0,0 +1,6 @@
+export type * as Unified from 'unified'
+export type { Plugin } from 'unified'
+export type * as Mdast from 'mdast'
+export type * as Hast from 'hast'
+
+export * from './remark/types'
