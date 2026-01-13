262e2bbc46fce74ed79d605ca162dd21433c807f
Ivo Dolenc <ivodolenc@gmail.com>
2026-01-13 20:14:45 +0100

    chore: format bundler config

--- packages/unplugins/rehype-highlight/bundler.config.ts
+++ packages/unplugins/rehype-highlight/bundler.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from '@hypernym/bundler'
 export default defineConfig({
   externals: [/^@sveltek/],
   entries: [
-    {
-      input: './src/index.ts',
-    },
+    { input: './src/index.ts' },
     {
       dts: './src/types.ts',
       output: './dist/index.d.ts',
