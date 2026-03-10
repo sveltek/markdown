7e3631d52b5387fcece63265b3a500ec77447f39
Ivo Dolenc <ivodolenc@gmail.com>
2026-03-10 17:04:08 +0100

    chore: add dts externals

--- packages/markdown/bundler.config.ts
+++ packages/markdown/bundler.config.ts
@@ -20,6 +20,13 @@ export default defineConfig({
       paths: [{ find: '@/shared', replacement: './shared/index.js' }],
     },
     {
+      externals: [
+        ...externals,
+        ...Object.keys(dependencies),
+        /^svelte/,
+        'hast',
+        'mdast',
+      ],
       dts: './src/types/index.ts',
       output: './dist/index.d.ts',
     },
